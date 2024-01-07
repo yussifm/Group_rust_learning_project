c43f8fbecb29b5cac05789f39201e2fb8397dd3e
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-01-07 17:38:30 +0000

    .

--- ml_ai_rust/src/main.rs
+++ ml_ai_rust/src/main.rs
@@ -1,8 +1,36 @@
 // We Matrix maths
 
+use crate::lib::{network::Network, activations::SIGMOID};
+
 pub mod lib;
 
+
+
+//  0, 0 --> 0
+//  0, 1 --> 1
+//  1, 0 --> 1
+//  1, 1 --> 0
 fn main() {
-    println!("Hello, world!");
+    println!("=========== Neural matrix Started ===========");
+    let inputs = vec![
+        vec![0.0, 0.0], // 0,0
+        vec![0.0, 1.0], // 0, 1
+        vec![1.0, 0.0], // 1,0
+        vec![1.0, 1.0], // 1,1
+    ];
+    let targets = vec![
+        vec![ 0.0], // 0
+        vec![ 1.0], // 1
+        vec![1.0], // 1
+        vec![0.0], // 0
+    ];
+
+    let mut network = Network::new_net(vec![2, 3, 1], 0.5, SIGMOID, );
+    network.train(inputs, targets, 10000);
+
+    println!("0 and 0: {:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("0 and 1: {:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("1 and 0: {:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("1 and 1: {:?}", network.feed_forward(vec![1.0, 1.0]));
 
 }
