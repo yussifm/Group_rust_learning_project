d903855a675b7192b11856d46550d2f791411742
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-02-01 01:29:50 +0000

    minigrep mvp done

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents){
+        println!("{line}");
+    }
+
+    Ok(())
+}
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "safe";
+        let contents = "\
+        Rust
+        safe, fast, productive. 
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,47 +1,25 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
 //    let config = parse_config(&args);
-   let config = Config::build(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {err}"); 
+   let config = Config::build(&args).unwrap_or_else(|_err| {
+  
     process::exit(1);
    });
    
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
+   if let Err(_e) =  run(config) {
+    
+    process::exit(1);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+   }
 
-    println!("With text:\n{contents}");
 }
-
-struct  Config{
-    query: String,
-    file_path: String,
-}
-
-impl  Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-          let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Ok(Config{query, file_path})
-    }
-}
-
-
-// fn parse_config(args: &[String]) -> Config {
-//    let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config{query, file_path}
-
-// }
