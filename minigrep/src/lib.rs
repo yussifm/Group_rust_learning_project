179976be178cbd72af2d27fe0888f5281a353a54
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-02-03 00:24:24 +0000

    completed

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -4,8 +4,14 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents){
-        println!("{line}");
+    if config.ignore_case {
+        for line in search_case_insensitive(&config.query, &contents) {
+            println!("{line}");
+        }
+    } else {
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
     }
 
     Ok(())
@@ -13,17 +19,37 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+        if args.len() < 4 {
             panic!("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let to_ignore_case = args[3].clone();
 
-        Ok(Config { query, file_path })
+        //  let ignore_case = env::var("IGNORE_CASE").is_ok();
+        // IGNORE_CASE=1 cargo run -- to poem.txt
+        // Powershell
+        // PS> $Env:IGNORE_CASE=1; cargo run -- to poem.txt
+
+        // This will make IGNORE_CASE persist for the remainder of your shell session. It can be unset with
+        // the Remove-Item cmdlet:
+        //PS> Remove-Item Env:IGNORE_CASE
+        
+        let mut ignore_case: bool = false;
+        if to_ignore_case.trim().to_lowercase() == "y" {
+            ignore_case = true;
+        }
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -34,12 +60,46 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
     results
-
+}
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+    results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "Pick";
+        let contents = "\
+        Rust
+        safe, fast, productive. 
+        Pick three.";
+
+        assert_eq!(vec!["Pick three."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "SaFe";
+        let contents = "\
+        Rust
+        safe, fast, productive. 
+        Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
+        );
+    }
 
     #[test]
     fn one_result() {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,20 +6,27 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    println!("================================================================");
+    println!(" ========== Searching for words in any Text file =========");
+    println!(" ========== word file.txt case-sensitive =========");
+    println!(" ========== Eg: rust rust_docs.text y =========");
+    println!(" ========== Eg: RuSt rust_docs.text N =========");
+     println!("================================================================");
 
-//    let config = parse_config(&args);
-   let config = Config::build(&args).unwrap_or_else(|_err| {
-  
-    process::exit(1);
-   });
-   
+    //    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // cargo run -- to poem.txt > output.txt
+        // cargo run > output.txt
+        eprintln!("Problem parsing arguments:{err}" );
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-   if let Err(_e) =  run(config) {
-    
-    process::exit(1);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 
-   }
-
+    println!("================================================================");
 }
