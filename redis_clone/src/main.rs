e2a39560142170100024d983e4ddd4a9e5c60f4f
Mohammed Yussif <mohammed.yussif@invennico.com>
2025-03-06 00:47:48 +0000

    step two

--- redis_clone/src/main.rs
+++ redis_clone/src/main.rs
@@ -22,6 +22,8 @@ fn handle_connection(stream: &mut TcpStream){
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
+    println!("Received: {:?}", buffer);
+
     let response = "+PONG\r\n";
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
