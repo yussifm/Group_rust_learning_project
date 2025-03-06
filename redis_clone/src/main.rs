d1cce279d2da62a54144829bb5c89bc19b0ecf82
Mohammed Yussif <mohammed.yussif@invennico.com>
2025-03-06 14:49:48 +0000

    adding tokio for async

--- redis_clone/src/main.rs
+++ redis_clone/src/main.rs
@@ -1,30 +1,56 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     let custom_address = "127.0.0.1:6379";
-    let listern = TcpListener::bind(custom_address).unwrap();
+    let listener = TcpListener::bind(custom_address).await?;
+    println!("Server running on {}", custom_address);
     
-    for stream in listern.incoming() {
-        match stream {
-            Ok(mut stream)=> {
-                handle_connection(&mut stream);
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, addr)) => {
+                println!("Connection accepted from: {}", addr);
+                tokio::spawn(async move {
+                    handle_connection(&mut stream).await;
+                });
             }
-            Err(e)=> {
-                println!("There was an error {e}");
+            Err(e) => {
+                println!("Error accepting connection: {}", e);
+                continue;
             }
-            
         }
     }
 }
 
-fn handle_connection(stream: &mut TcpStream){
+async fn handle_connection(stream: &mut TcpStream) {
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
 
-    println!("Received: {:?}", buffer);
-
-    let response = "+PONG\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(size) if size != 0 => {
+                println!("Received: {:?}", &buffer[..size]);
+                let response = "+PONG\r\n";
+                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                    eprintln!("Error writing to socket: {}", e);
+                    break;
+                }
+                if let Err(e) = stream.flush().await {
+                    eprintln!("Error flushing socket: {}", e);
+                    break;
+                }
+                println!("Sent response: {}", response.trim());
+            }
+            Ok(_) => {
+                println!("Connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("Error reading from socket: {}", e);
+                break;
+            }
+        }
+    }
 }
