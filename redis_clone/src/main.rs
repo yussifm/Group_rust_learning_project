93fed33bd5da1720b748cf687f2a6fbf97ed977c
Mohammed Yussif <mohammed.yussif@invennico.com>
2025-03-06 00:35:00 +0000

    .

--- redis_clone/src/main.rs
+++ redis_clone/src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let custom_address = "127.0.0.1:6379";
+    let listern = TcpListener::bind(custom_address).unwrap();
+    
+    for stream in listern.incoming() {
+        match stream {
+            Ok(mut stream)=> {
+                handle_connection(&mut stream);
+            }
+            Err(e)=> {
+                println!("There was an error {e}");
+            }
+            
+        }
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream){
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
